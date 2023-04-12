bc47acb198783dcfe117bee285a92bb09ba2fb53
seulgi99 <tmfrk0426@gmail.com>
2023-04-12 21:44:49 +0900

    chore : 초기세팅

---
+++ deep_look/mypage/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render,redirect
+from rest_framework import viewsets, permissions, generics, status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.decorators import api_view
+from rest_framework.generics import get_object_or_404
+from django.core import serializers
+
+# Create your views here.
+
+class MypageAPI(APIView):
+    def get(self):
+        return
